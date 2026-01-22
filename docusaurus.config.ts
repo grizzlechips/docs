3e7f0ea81abfe216ee5d38382191fac15f127fef
grizzlechips <zach@zachtuttle.com>
2026-01-22 09:09:56 -0500

    Refactor markdown configuration in docusaurus.config.ts
    - Moved markdown hooks directly under the markdown key for improved clarity and organization, removing the unnecessary customFields wrapper.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,12 +53,8 @@ const config: Config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  customFields: {
-    markdown: {
-      hooks: {
-        onBrokenMarkdownLinks: "warn",
-      },
-    },
+  markdown: {
+    onBrokenMarkdownLinks: "warn",
   },
   i18n: {
     defaultLocale: DEFAULT_LOCALE,
